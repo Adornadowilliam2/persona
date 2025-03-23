640301c42fe957d1ed12824c880f0b1750de10fa
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-03-23 08:47:13 +0800

    adding a design to the contactus

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import contactus from '../assets/contactus.png';
+import { ContactMail } from '@mui/icons-material'; // Import Material UI icon
 
 export default function Contact() {
+  const [showDetails, setShowDetails] = useState(false);
+  
   return (
     <div>
-        <Navbar />
-        <h1>Contact</h1>
+      <Navbar setShowDetails={setShowDetails} showDetails={showDetails} />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: '20px',
+          backgroundColor: '#111111',
+          color: 'white',
+        }}
+      >
+        <span style={{ padding: '50px' }}>
+          <h1>Get in Touch</h1>
+          <p>If you have any questions or feedback, please don't hesitate to contact us.</p>
+        </span>
+        <img src={contactus} alt="Contact Us" />
+      </div>
+      <div>
+        <div
+          className="contact-us-card"
+          style={{
+            padding: '5px',
+            backgroundColor: '#f1f1f1',
+     
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: '8px',
+            flexDirection: 'column',
+          
+          }}
+        >
+          <ContactMail style={{ fontSize: '40px', marginRight: '10px' }} />
+          <h1>Contact Information</h1>   <p>
+            <strong>Phone:</strong> +81 90 1234 5678 
+          </p>
+          <p style={{marginBottom: '10px'}}>
+            <strong>Email:</strong> support@persona.jp
+          </p>
+
+         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3240.0770297793506!2d139.771111!3d35.699722!3m2!1i1024!2i768!4f13.1!4m3!3e6!4m0!4m0!5e0!3m2!1sen!2sph!4v1742689539884!5m2!1sen!2sph" id='map'  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        
+        </div>
+
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 
 export default function Features() {
+  const [showDetails, setShowDetails] = useState(false);
   return (
     <div>
-        <Navbar />
+        <Navbar setShowDetails={setShowDetails} showDetails={showDetails} />
         <h1>Features</h1>
     </div>
   )
